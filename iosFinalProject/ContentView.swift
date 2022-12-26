fe981707d6c1e54e8d760030176ac99668390158
henry0523ho <henry0523ho.3@gmail.com>
2022-12-26 15:22:03 +0800

    Initial Commit

---
+++ iosFinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosFinalProject
+//
+//  Created by User10 on 26/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosFinalProject/iosFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosFinalProjectApp.swift
+//  iosFinalProject
+//
+//  Created by User10 on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iosFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
